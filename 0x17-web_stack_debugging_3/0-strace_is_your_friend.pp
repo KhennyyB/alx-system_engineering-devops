1e48f055fee8244962d1c1cd8f60086caf968cdc
Okoli Ekene <khennyphresh@gmail.com>
2022-11-03 08:16:57 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,11 @@
-# Fixes a faulty wordpress site
-exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-/var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+# fix file extension typo
+exec { 'replace phpp with php':
+  command  => 'sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
+  provider => shell
+}
+
+#restart apache2
+exec { 'restart apache2 service':
+  command  => 'service apache2 restart',
+  provider => shell
 }
